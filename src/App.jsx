40fdde954e5ca94339d1b90c14e3885e80ec0815
edgrrx15 <edgargomezdlsantos@gmail.com>
2024-04-11 14:57:37 -0700

    verficar comentario

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//Profe esto esta medio raros
 function App() {
   const [count, setCount] = useState(0)
 
